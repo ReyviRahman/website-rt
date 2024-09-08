f65384bf7c440c3494d3b15cdcc8df7d5a9d3b7a
Reyvi Rahman <reyvirahman@gmail.com>
2024-09-08 15:43:29 +0700

    change cors origin

--- server/index.js
+++ server/index.js
@@ -10,7 +10,11 @@ if (process.env.NODE_ENV != "production") {
 require("dotenv").config();
 
 const app = express()
-app.use(cors())
+
+app.use(cors({
+  origin: 'https://website-rt.vercel.app'
+}))
+
 app.use(express.json())
 
 connectToDb()
